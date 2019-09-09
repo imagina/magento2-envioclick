b87f98e7cda2bac362010e26e7d48194fdef6964
William Verde <williamverde10@gmail.com>
2019-09-09 15:22:15 -0400

    fixes

--- Model/Carrier/Customshipping.php
+++ Model/Carrier/Customshipping.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Imagina\Customshipping\Model\Carrier;
+namespace Imagina\Envioclick\Model\Carrier;
 
 use Magento\Quote\Model\Quote\Address\RateRequest;
 use Magento\Shipping\Model\Carrier\AbstractCarrier;
